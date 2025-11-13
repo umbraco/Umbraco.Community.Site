55aacac85c49b1c4c1b0e06bf4981e1419189db7
Laura Weatherhead <laura@spun.dev>
2025-11-13 10:08:54 +0000

    Feature/styling configuration (#21)

--- src/UmbracoCommunity.Web/Models/ViewModels/Blocks/BlockGridRow.cs
+++ src/UmbracoCommunity.Web/Models/ViewModels/Blocks/BlockGridRow.cs
@@ -7,6 +7,9 @@ namespace UmbracoCommunity.Web.Models.ViewModels.Blocks
     {
         public IList<BlockGridItem> Blocks { get; set; } = [];
         public bool HasMultipleBlocks { get; set; } = false;
+
+        public bool HasBg { get; set; } = false;
+        public string? BackgroundColour { get; set; }
         public string Alias => GetAlias();
         public string IdHash { get; } = StringUtilities.RandomString(5);
 
