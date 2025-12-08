3152676f0d7b86304c0516b96f659a7a7f1674a4
Sebastiaan Janssen <sebastiaan@umbraco.com>
2025-12-08 15:36:15 +0100

    Dashboard for contributions to the CMS (#46)
    Ability to filter on a date range and see contributions per release

---
+++ src/UmbracoCommunity.Web/Features/GitHubUsers/Models/ContributorDetail.cs
@@ -0,0 +1,8 @@
+﻿namespace UmbracoCommunity.Web.Features.GitHubUsers.Api.Models;
+
+public class ContributorDetail
+{
+    public string Login { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public int PullRequestCount { get; set; }
+}
