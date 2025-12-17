2daed731f53234cc56cf46d78a6209b96a96f852
Laura Weatherhead <laura@spun.dev>
2025-12-17 14:56:11 +0000

    Fix video image issues (#55)
    * Ensure crop sizes and attempt yt video fix
    
    * Let yt video play when click the poster image

--- src/UmbracoCommunity.StaticAssets/src/components/video-pickers/video-picker-youtube.ts
+++ src/UmbracoCommunity.StaticAssets/src/components/video-pickers/video-picker-youtube.ts
@@ -1,13 +1,41 @@
 import { customElement } from 'lit/decorators.js';
 import { DcVideoBlockElement } from './video-block.element';
+import { YouTubePlayer } from 'youtube-player/dist/types';
+import PlayerFactory from 'youtube-player/dist';
+
+declare const YT: any;
+let player: any;
 
 const elementName = "dc-video-picker-youtube";
 
 @customElement(elementName)
 export class VideoPickerYouTubeElement extends DcVideoBlockElement {
+    connectedCallback(): void {
+        super.connectedCallback();
+
+        if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') {
+            var tag = document.createElement('script');
+            tag.src = "https://www.youtube.com/iframe_api";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            if (firstScriptTag && firstScriptTag.parentNode) {
+                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+            }
+        }
+    }
+    
     playVideo() {
         this.videoPlayed = true;
-    };    
+
+        player = new YT.Player('player', {
+            events: {
+                'onReady': this.onPlayerReady
+            }
+        });
+    };
+
+    onPlayerReady(event: any) {
+        player.playVideo();
+    }
 }
 
 declare global {
