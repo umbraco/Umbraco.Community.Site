09f84956b41e38df3a3d24a1ede3a17a8028945c
Sebastiaan Janssen <sebastiaan@umbraco.com>
2026-01-16 14:22:10 +0100

    Fix broken HQ member management

--- src/UmbracoCommunity.Web/Features/GitHubSync/Models/GitHubHqMember.cs
+++ src/UmbracoCommunity.Web/Features/GitHubSync/Models/GitHubHqMember.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 
 namespace UmbracoCommunity.Web.Features.GitHubSync.Models;
@@ -5,14 +6,24 @@ namespace UmbracoCommunity.Web.Features.GitHubSync.Models;
 public class GitHubHqMember
 {
     [JsonProperty("id")]
+    [JsonPropertyName("id")]
     public string Id { get; set; } = string.Empty;
+
+    [JsonPropertyName("login")]
     public string Login { get; set; } = string.Empty;
+
+    [JsonPropertyName("name")]
     public string Name { get; set; } = string.Empty;
+
+    [JsonPropertyName("periods")]
     public List<EmploymentPeriod> Periods { get; set; } = new();
 }
 
 public class EmploymentPeriod
 {
+    [JsonPropertyName("start")]
     public DateTime? Start { get; set; }
+
+    [JsonPropertyName("end")]
     public DateTime? End { get; set; }
 }
