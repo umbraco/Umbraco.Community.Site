d4b50f4e9f80ccf02f09195665fd3d621d0a8ef6
Laura Weatherhead <lsw@umbraco.dk>
2026-02-10 09:45:49 +0000

    Missed during merge

--- src/UmbracoCommunity.Web/Models/Pages/BlogPageViewModel.cs
+++ src/UmbracoCommunity.Web/Models/Pages/BlogPageViewModel.cs
@@ -4,8 +4,6 @@ namespace UmbracoCommunity.Web.Models.Pages
 {
     public class BlogPageViewModel(IPublishedContent currentPage) : PageViewModelBase(currentPage)
     {
-        public string? RssPath { get; set; }
-
         public int PageSize { get; set; } = 10;
     }
 }
