9189ba2ac57793d4c676e720246932b52a80bd6e
Sebastiaan Janssen <sebastiaan@umbraco.com>
2026-02-17 14:04:29 +0100

    Remove the "hello" and "goodbye" debugging messages and the now redundant entrypoint for it.

--- src/UmbracoCommunity.Extensions/Client/src/bundle.manifests.ts
+++ src/UmbracoCommunity.Extensions/Client/src/bundle.manifests.ts
@@ -1,4 +1,3 @@
-import { manifests as entrypoints } from "./entrypoints/manifest.js";
 import { manifests as dashboards } from "./dashboards/manifest.js";
 import { manifests as conditions } from "./conditions/manifest.js";
 import { manifests as entityActions } from "./entity-actions/manifest.js";
@@ -6,7 +5,6 @@ import { manifests as entityActions } from "./entity-actions/manifest.js";
 // Job of the bundle is to collate all the manifests from different parts of the extension and load other manifests
 // We load this bundle from umbraco-package.json
 export const manifests: Array<UmbExtensionManifest> = [
-  ...entrypoints,
   ...dashboards,
   ...conditions,
   ...entityActions,
--- src/UmbracoCommunity.Extensions/Client/src/entrypoints/manifest.ts
+++
@@ -1,8 +0,0 @@
-export const manifests: Array<UmbExtensionManifest> = [
-  {
-    name: "Umbraco Community Extensions Entrypoint",
-    alias: "UmbracoCommunity.Extensions.Entrypoint",
-    type: "backofficeEntryPoint",
-    js: () => import("./entrypoint.js"),
-  },
-];
