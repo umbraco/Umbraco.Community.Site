fd6127176fc7073dcb68d922a7d04a5852d2ff4d
Laura Weatherhead <lsw@umbraco.dk>
2026-02-25 15:34:37 +0000

    Security fixes and validation

--- src/UmbracoCommunity.StaticAssets/src/util/index.ts
+++ src/UmbracoCommunity.StaticAssets/src/util/index.ts
@@ -2,3 +2,5 @@ export { default as Cookie } from "./cookie.js";
 export * from "./utm-helper.js";
 export { default as UtmParams } from "./utm-helper.js";
 export { default as LocaleResolver } from "./locale-resolver.js";
+export { withRetry, isRetryableError } from "./retry.js";
+export type { RetryOptions, RetryError } from "./retry.js";
--- src/UmbracoCommunity.Web.UI/Program.cs
+++ src/UmbracoCommunity.Web.UI/Program.cs
@@ -1,4 +1,5 @@
 using UmbracoCommunity.Web.Extensions;
+using UmbracoCommunity.Web.Middleware;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -12,13 +13,33 @@ builder.CreateUmbracoBuilder()
     .AddPipelineFilters()
     .Build();
 
+builder.Services.AddResponseCaching();
+
 WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/error.html");
+    app.UseHsts();
+}
+
+app.UseWhen(
+    // Media requests excluded from response caching because of ImageSharp caching duplication and issues with caching query-parametrized images.
+    n => !n.Request.Path.StartsWithSegments("/media", StringComparison.OrdinalIgnoreCase),
+    appBuilder => appBuilder.UseResponseCaching()
+);
 
 await app.BootUmbracoAsync();
 
 app.UseHttpsRedirection();
 
 app.UseSecurityHeaders();
+
+app.UseMiddleware<FormValidationMiddleware>();
 
 app.UseOutputCache();
 
