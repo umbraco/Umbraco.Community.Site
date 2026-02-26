5dcc60a442e260ce62c92cde18abebca8ad214b3
Laura Weatherhead <laura@spun.dev>
2026-02-26 09:27:15 +0000

    Fix blog date rendering with page culture instead of browser (#79)

---
+++ src/UmbracoCommunity.Web/Constants.Culture.cs
@@ -0,0 +1,10 @@
+namespace UmbracoCommunity.Web
+{
+    public static partial class Constants
+    {
+        public static class Culture
+        {
+            public const string Default = "en-GB";
+        }
+    }
+}
